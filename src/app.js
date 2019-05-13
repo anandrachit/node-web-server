0def079589627b3c89594d6c745f2c70981135ed
Rachit Anand <rachitanand@ip-63-38.wvu.edu>
2019-05-13 16:18:56 -0400

    Setup App for Heroku

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ const forecast = require('./utils/forecast');
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 const viewsPath = path.join(__dirname, '../templates/views');
 const partialsPath = path.join(__dirname, '../templates/partials');
 
@@ -87,6 +89,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, ()=> {
-    console.log('Running on port 3000');
+app.listen(PORT, ()=> {
+    console.log(`Running on port ${PORT}`);
 });
